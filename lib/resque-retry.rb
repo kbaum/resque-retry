c2599e569a04487e06859789d0ac32f494f6a113
Karl <karl@weshopnetwork.com>
2010-07-13 15:59:02 -0400

    added resque-retry module

--- lib/resque-retry.rb
+++ lib/resque-retry.rb
@@ -3,5 +3,5 @@ require 'resque_scheduler'
 
 require 'resque/plugins/retry'
 require 'resque/plugins/exponential_backoff'
-require 'resque/plugins/retry_failure_backend'
+require 'resque-retry/retry_failure_backend'
 require 'resque-retry/server'
